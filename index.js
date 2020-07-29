fb8f90298983982b02b0a8118e9c262ed3979d72
Caitlin Oriel <croriel@gmail.com>
2020-07-29 22:34:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase(string))
 }
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
